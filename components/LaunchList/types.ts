d368bc25c6697f6c19b3c0f3c1abb2e86b0df5b0
Andrej Tlcina <andrej.tlcina23@gmail.com>
2022-09-07 19:57:15 +0200

    remove types from gitignore

---
+++ components/LaunchList/types.ts
@@ -0,0 +1,4 @@
+export type Display = "list" | "grid";
+export type Order = "asc" | "desc";
+export type Sort = "mission_name" | "site_name_long";
+export type Filters = { order: Order; sort: Sort };
